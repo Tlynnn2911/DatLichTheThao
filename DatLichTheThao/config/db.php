0c2b6813109c40989c319f93c8bd0b3196df081d
Tlynnn2911 <linhmun2911@gmail.com>
2026-04-07 20:24:16 +0700

    fix db connection for railway

--- DatLichTheThao/config/db.php
+++ DatLichTheThao/config/db.php
@@ -1,12 +1,15 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "booklich";
+$host = getenv("MYSQLHOST");
+$user = getenv("MYSQLUSER");
+$pass = getenv("MYSQLPASSWORD");
+$db   = getenv("MYSQLDATABASE");
+$port = getenv("MYSQLPORT");
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("Kết nối thất bại: " . $conn->connect_error);
 }
+
 $conn->set_charset("utf8mb4");
+?>
